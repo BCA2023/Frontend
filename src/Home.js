02347a3af3350e73358e29a60d26f6ba175bcdf2
BCA2023 <202100977@sigc.edu>
2024-03-09 04:10:25 -0800

    update

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="https://d37y9b4kfek2hl.cloudfront.net/services/react-js-development-services.png" alt="Welcome picture" />
+      <img className="home-img" src="https://media.istockphoto.com/id/1349328573/photo/happy-asia-athletic-woman-using-fitness-tracker-while-running-on-treadmill-in-a-gym.jpg?s=612x612&w=0&k=20&c=4SOK1GPOlYmTIO8lZcnNF-717pgvtbE9VE3-9sdslc8=" alt="Welcome picture" />
     </div>
   )
 }
